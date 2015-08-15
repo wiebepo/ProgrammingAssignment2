672bb4a2e9ae254b1b2b2be87eef46e6b6e5e7bb
wiebepo <wiebepo@gmail.com>
2015-08-15 08:08:16 -0500

    Assignment 2
    Complete

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## leverage the lexical nature of the namespace to improve performance via caching
 
-## Write a short comment describing this function
+## Create functions and namespace associated with the matrix for caching the matrix inversion
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     i <- NULL
+     set <- function(y) {
+         x <<- y
+         i <<- NULL
+       }
+     get <- function() x
+     setinv <- function(inv) i <<- inv
+     getinv <- function() i
+     list(set = set, get = get,
+                   setinv = setinv,
+                   getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## Return the cached matrix inversion if available, otherwise calculate
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+     i <- x$getinv()
+     if(is.null(i)) {
+       data <- x$get()
+       i <- solve(data)
+       x$setinv(i)
+       }
+     i
 }
